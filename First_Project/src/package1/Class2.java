42169317874978d7bf758a9d0adec7f0d6988cb9
Shree Mishra <Shree Mishra>
2017-01-11 21:30:41 -0500

    repository to add practising codes in java

---
+++ First_Project/src/package1/Class2.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Class2 extends Class3{
+	
+	Class3 hu = new Class2();
+	hu.age =
+	
+ 
+	
+	
+}
---
+++ First_Project/src/package1/Class3.java
@@ -0,0 +1,33 @@
+package package1;
+
+public class Class3 {
+
+	
+	private int age ;
+	private int salary;
+	
+	
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	void sal(){
+		System.out.println(age);
+	}
+}
